c33e9987cf40409fed790be4a349d1d4be1038b0
Liam <hoailinh1210@gmail.com>
2023-04-04 00:12:15 +0700

    feat: message content with i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,22 +1,25 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import translationEN from './public/locales/en/common.json';
+import commonEN from './public/locales/en/common.json';
+import messageEN from './public/locales/en/message.json';
 
 // the translations
 const resources = {
   en: {
-    translation: translationEN,
+    common: commonEN,
+    message: messageEN,
   },
 };
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+    ns: ['common', 'message'], // add the namespaces you want to use
     resources,
+    defaultNS: 'common',
+    lng: 'en',
     fallbackLng: 'en', // use en if detected lng is not available
-
-    keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
       escapeValue: false, // react already safes from xss
