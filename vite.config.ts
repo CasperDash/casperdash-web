484f5f210614d49c8d166761c7639b46da36e9bb
Liam <20489407+hoailinh1210@users.noreply.github.com>
2023-04-03 15:53:58 +0700

    CI: Deploy Testnet & Update typo (#23)

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,37 @@
 import { resolve as pathResolve } from 'path';
 
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgr from 'vite-plugin-svgr';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const resolve = (path: string) => pathResolve(__dirname, path);
+
+// remove shebeng from code (most often #!/usr/bin/env node)
+// vite recognizes the shebang as a comment and needs to be removed because it causes a parse error.
+export const removeShebangPlugin = (): Plugin => {
+  return {
+    name: 'vitest:remove-shebang-plugin',
+    enforce: 'pre',
+    transform(code) {
+      // eslint-disable-next-line no-useless-escape
+      return code.replace(/^\#\!.*/, '');
+    },
+  };
+};
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
-    viteTsConfigPaths({
-      root: './',
-    }),
+    removeShebangPlugin(),
     react(),
     svgr(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
   ],
   resolve: {
     alias: {
@@ -37,6 +52,6 @@ export default defineConfig({
   define: {
     // By default, Vite doesn't include shims for NodeJS/
     // necessary for segment analytics lib to work
-    global: {},
+    // global: {},
   },
 });
