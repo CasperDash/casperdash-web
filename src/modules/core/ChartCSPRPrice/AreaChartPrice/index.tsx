747dc5d1fd8dc5abaca8bc02d545d1538968788b
Liam <20489407+hoailinh1210@users.noreply.github.com>
2023-04-04 22:38:02 +0700

    refactor: responsive chart area (#30)

--- src/modules/core/ChartCSPRPrice/AreaChartPrice/index.tsx
+++ src/modules/core/ChartCSPRPrice/AreaChartPrice/index.tsx
@@ -1,25 +1,14 @@
 import { Box } from '@chakra-ui/react';
 import dayjs from 'dayjs';
-import { Area, AreaChart, Tooltip, TooltipProps } from 'recharts';
+import {
+  Area,
+  AreaChart,
+  ResponsiveContainer,
+  Tooltip,
+  TooltipProps,
+} from 'recharts';
 
 import { useGetCoingeckoHistories } from '@/hooks/queries/useGetCoingeckoHistories';
-
-// const data: { year: number; value: number }[] = [];
-
-// const rand = 300;
-// for (let i = 0; i < 7; i++) {
-//   const d = {
-//     year: 2000 + i,
-//     value: Math.random() * (rand + 50) + 100,
-//   };
-
-//   data.push(d);
-// }
-
-type Props = {
-  width?: number;
-  height?: number;
-};
 
 const CustomTooltip = ({ active, payload }: TooltipProps<string, string>) => {
   if (active && payload && payload.length) {
@@ -34,31 +23,28 @@ const CustomTooltip = ({ active, payload }: TooltipProps<string, string>) => {
   return null;
 };
 
-const AreaChartPrice = ({ width = 500, height = 200 }: Props) => {
+const AreaChartPrice = () => {
   const { data } = useGetCoingeckoHistories();
 
   return (
-    <AreaChart
-      width={width}
-      height={height}
-      data={data || []}
-      margin={{ top: 5, right: 20, bottom: 30, left: 0 }}
-    >
-      <defs>
-        <linearGradient id="coinHistory" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="5%" stopColor="#58BD7D" stopOpacity={0.8} />
-          <stop offset="95%" stopColor="#58BD7D" stopOpacity={0} />
-        </linearGradient>
-      </defs>
-      <Tooltip content={<CustomTooltip />} />
-      <Area
-        type="monotone"
-        dataKey="value"
-        stroke="#58BD7D"
-        fillOpacity={1}
-        fill="url(#coinHistory)"
-      />
-    </AreaChart>
+    <ResponsiveContainer height={200} width="100%">
+      <AreaChart data={data || []} margin={{ bottom: 30 }}>
+        <defs>
+          <linearGradient id="coinHistory" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#58BD7D" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#58BD7D" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <Tooltip content={<CustomTooltip />} />
+        <Area
+          type="monotone"
+          dataKey="value"
+          stroke="#58BD7D"
+          fillOpacity={1}
+          fill="url(#coinHistory)"
+        />
+      </AreaChart>
+    </ResponsiveContainer>
   );
 };
 
