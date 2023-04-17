4c3497b931f3bc3cd4bf070fbd41d7b85722c736
Liam <20489407+hoailinh1210@users.noreply.github.com>
2023-04-17 21:31:46 +0700

    refactor: popup sdk module (#38)

--- src/modules/PopupSDK/index.tsx
+++ src/modules/PopupSDK/index.tsx
@@ -1,7 +1,105 @@
-import ModalSDKAction from './components/ModalSDKAction';
+import { useEffect, useState } from 'react';
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+  ModalFooter,
+  Divider,
+  useDisclosure,
+  ModalHeader,
+  Heading,
+} from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+import { useSearchParams } from 'react-router-dom';
+
+import { RepliedMessageMethodEnums } from '@/enums/postMessageMethod';
+import { useAccount } from '@/hooks/useAccount';
+import { useAutoSetOriginUrl } from '@/hooks/useAutoSetOriginUrl';
+import { useWatchMessageEvent } from '@/hooks/useWatchMessageEvent';
+import SDKConnectWallet from '@/modules/PopupSDK/components/ConnectWallet';
+import SDKSign from '@/modules/PopupSDK/components/Sign';
+import SDKSignMessage from '@/modules/PopupSDK/components/SignMessage';
+import {
+  normalizeSignDeployParams,
+  normalizeSignMessageParams,
+} from '@/utils/normalizer';
 
 const PopupSDK = () => {
-  return <ModalSDKAction />;
+  const { publicKey } = useAccount();
+  const { t } = useTranslation();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [currentMethod, setCurrentMethod] = useState<RepliedMessageMethodEnums>(
+    RepliedMessageMethodEnums.CONNECT
+  );
+  const [currentParams, setCurrentParams] = useState({});
+  const [searchParams] = useSearchParams();
+  useAutoSetOriginUrl();
+
+  useWatchMessageEvent({
+    onHandle: (
+      method: RepliedMessageMethodEnums,
+      params: Record<string, string | undefined>
+    ) => {
+      setCurrentMethod(method);
+      setCurrentParams(params);
+      onOpen();
+    },
+  });
+
+  useEffect(() => {
+    const originUrl = searchParams.get('originUrl');
+    if (originUrl && publicKey) {
+      onOpen();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [publicKey]);
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent borderRadius="2xl" w={{ base: '340px', md: '400px' }}>
+          <ModalHeader>
+            <Heading variant="xl" textAlign="center">
+              {currentMethod === RepliedMessageMethodEnums.CONNECT
+                ? t('connect_with_your_site')
+                : t('signature_request')}
+            </Heading>
+          </ModalHeader>
+          <Divider />
+          <ModalCloseButton />
+          <ModalBody>
+            {
+              {
+                [RepliedMessageMethodEnums.CONNECT]: (
+                  <SDKConnectWallet onSuccess={() => onClose()} />
+                ),
+                [RepliedMessageMethodEnums.SIGN]: (
+                  <SDKSign
+                    params={normalizeSignDeployParams(currentParams)}
+                    onApproved={onClose}
+                    onRejected={onClose}
+                  />
+                ),
+                [RepliedMessageMethodEnums.SIGN_MESSAGE]: (
+                  <SDKSignMessage
+                    params={normalizeSignMessageParams(currentParams)}
+                    onApproved={onClose}
+                    onRejected={onClose}
+                  />
+                ),
+                [RepliedMessageMethodEnums.DISCONNECT]: null,
+              }[currentMethod]
+            }
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default PopupSDK;
