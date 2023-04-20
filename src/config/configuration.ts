f9608ac039aba97d34d08c5f19d7daccd17fee12
Liam <hoailinh1210@gmail.com>
2023-04-21 00:36:31 +0700

    feat: vite env config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,9 @@
+const { VITE_CASPERDASH_BASE_API_URL, VITE_NETWORK_NAME } = import.meta.env;
+
 export const Config = {
   coingeckoBaseUrl: 'https://api.coingecko.com/api/v3',
-  casperDashBaseUrl: 'https://api.casperdash.io',
-  networkName: 'casper-test',
+  casperDashBaseUrl:
+    VITE_CASPERDASH_BASE_API_URL || 'https://api.casperdash.io',
+  networkName: VITE_NETWORK_NAME || 'casper',
   csprLiveUrl: 'https://testnet.cspr.live',
 };
