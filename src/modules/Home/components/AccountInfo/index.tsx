dee007577d3c55b3759f4fb6017b747668cb68b9
Liam <hoailinh1210@gmail.com>
2023-04-26 23:05:25 +0700

    refactor: account management on homepage

--- src/modules/Home/components/AccountInfo/index.tsx
+++ src/modules/Home/components/AccountInfo/index.tsx
@@ -1,21 +1,52 @@
+import { CopyIcon, EditIcon } from '@chakra-ui/icons';
 import {
   Box,
   BoxProps,
   Flex,
-  Heading,
   Text,
   useClipboard,
+  useDisclosure,
 } from '@chakra-ui/react';
 
 import { useI18nToast } from '@/hooks/helpers/useI18nToast';
 import { useGetCurrentAccount } from '@/hooks/queries/useGetCurrentAccount';
+import { ModalAccounts } from '@/modules/core/ModalAccounts';
+import UnlockWalletPopupRequired from '@/modules/core/UnlockWalletPopupRequired';
 
 export type AccountInfoProps = BoxProps;
 
+const AccountName = () => {
+  const { data: { name } = {} } = useGetCurrentAccount();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Flex
+        alignItems={'center'}
+        cursor="pointer"
+        _hover={{
+          color: 'secondary',
+        }}
+        onClick={onOpen}
+      >
+        <Text fontSize={{ base: 'xl', md: '2xl' }}>{name}</Text>
+        <EditIcon ml="2" />
+      </Flex>
+      <UnlockWalletPopupRequired>
+        <ModalAccounts isOpen={isOpen} onClose={onClose} />
+      </UnlockWalletPopupRequired>
+    </>
+  );
+};
+
 const AccountInfo = (props: AccountInfoProps) => {
-  const { data: { publicKey, name } = {} } = useGetCurrentAccount();
+  const { data: { publicKey } = {} } = useGetCurrentAccount();
   const { toastSuccess } = useI18nToast();
   const { onCopy } = useClipboard(publicKey || '');
+
+  if (!publicKey) {
+    return null;
+  }
 
   const handleOnCopy = () => {
     onCopy();
@@ -33,16 +64,12 @@ const AccountInfo = (props: AccountInfoProps) => {
     >
       <Flex alignItems="center" justifyContent="space-between" gap="4">
         <Box flex={'1 0 100px'}>
-          <Heading as="h4" size={{ base: 'sm', md: 'xl' }}>
-            {name}
-          </Heading>
+          <AccountName />
         </Box>
-
-        {publicKey && (
-          <Text noOfLines={3} onClick={handleOnCopy}>
-            {publicKey}
-          </Text>
-        )}
+        <Flex alignItems={'center'}>
+          <Text noOfLines={3}>{publicKey}</Text>
+          <CopyIcon ml="4" onClick={handleOnCopy} cursor="pointer" />
+        </Flex>
       </Flex>
     </Box>
   );
