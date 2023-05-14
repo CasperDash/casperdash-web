1d0a39c4aeb83f59f8b5fe9b8a71538f891361b5
Liam <hoailinh1210@gmail.com>
2023-05-15 00:11:14 +0700

    nit

--- src/theme/foundations/radii.ts
+++ src/theme/foundations/radii.ts
@@ -4,10 +4,10 @@ const radii = {
   base: '0.25rem',
   md: '0.375rem',
   lg: '0.5rem',
-  xl: '0.75rem',
-  '2xl': '1rem',
-  '3xl': '1.5rem',
-  '4xl': '1.75rem',
+  xl: '0.75rem', // 12px
+  '2xl': '1rem', // 16px
+  '3xl': '1.5rem', // 24px
+  '4xl': '1.75rem', // 28px
   '5xl': '2rem',
   full: '9999px',
 };
