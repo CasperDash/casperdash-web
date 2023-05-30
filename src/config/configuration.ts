bc8166d9116b7eb79669eb161b7f1df4a51afe11
Liam <hoailinh1210@gmail.com>
2023-05-30 12:46:05 +0700

    ci: test deploy testnet

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,9 +1,10 @@
-const { VITE_CASPERDASH_BASE_API_URL, VITE_NETWORK_NAME } = import.meta.env;
+const { VITE_CASPERDASH_BASE_API_URL, VITE_NETWORK_NAME, VITE_CSPR_LIVE_URL } =
+  import.meta.env;
 
 export const Config = {
   coingeckoBaseUrl: 'https://api.coingecko.com/api/v3',
   casperDashBaseUrl:
     VITE_CASPERDASH_BASE_API_URL || 'https://api.casperdash.io',
   networkName: VITE_NETWORK_NAME || 'casper',
-  csprLiveUrl: 'https://testnet.cspr.live',
+  csprLiveUrl: VITE_CSPR_LIVE_URL || 'https://testnet.cspr.live',
 };
